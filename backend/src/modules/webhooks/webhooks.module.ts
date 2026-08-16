537efa550cff1e9353037f5b9cb2bd3169d5b5b3
kauediniz-dev <dinizkaue26@gmail.com>
2026-08-16 00:00:59 -0300

    feat(webhooks): implement idempotent payment event processing (#6)

--- backend/src/modules/webhooks/webhooks.module.ts
+++ backend/src/modules/webhooks/webhooks.module.ts
@@ -2,8 +2,14 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { WebhookEvent } from './entities/webhook-event.entity';
+import { WebhooksController } from './webhooks.controller';
+import { WebhooksService } from './webhooks.service';
+import { Order } from '../orders/entities/order.entity';
+import { Transaction } from '../transactions/entities/transaction.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([WebhookEvent])],
+  imports: [TypeOrmModule.forFeature([WebhookEvent, Order, Transaction])],
+  controllers: [WebhooksController],
+  providers: [WebhooksService],
 })
 export class WebhooksModule {}
